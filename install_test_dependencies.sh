5fcf02ff40640e2777f0082f3af48c13fea49dff
tehw0lf <tehwolf@protonmail.com>
2025-08-10 18:09:53 +0200

    Install whisper via global pip

--- install_test_dependencies.sh
+++ install_test_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install curl ffmpeg python3-openai-whisper
+sudo apt-get update
+sudo apt-get install curl ffmpeg
+sudo pip install -U openai-whisper
 curl -LsSf https://astral.sh/uv/install.sh | sh
